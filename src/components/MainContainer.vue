7d4483ae8a55817ee4e52ae0ec8d71918d25fa4f
LiZhongpeng2 <lizhongpeng2@gmail.com>
2025-08-16 11:19:22 +0800

    update: full GUI interface

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="selector">
+    <div class="radio-language">
+      <input type="radio" id="en" value="English" v-model="selectedLanguage" />
+      <label for="en">English</label>
+
+      <input type="radio" id="ch" value="Chinese" v-model="selectedLanguage" />
+      <label for="ch">中文</label>
+    </div>
+
+    <div class="radio-model">
+      <input type="radio" id="RE" value="RE" v-model="selectedModel" />
+      <label for="RE">RE</label>
+
+      <input type="radio" id="NER" value="NER" v-model="selectedModel" />
+      <label for="RE">NER</label>
+
+      <input type="radio" id="EE" value="EE" v-model="selectedModel" />
+      <label for="RE">EE</label>
+    </div>
+  </div>
+
+  <div class="content-container">
+    <input type="text" placeholder="sentence" v-model="sentence" />
+    <input
+      type="text"
+      placeholder="relation type list (rtl)*"
+      v-model="rtl_input"
+    />
+    <input
+      type="text"
+      placeholder="subject type list (stl)*"
+      v-model="stl_input"
+    />
+    <input
+      type="text"
+      placeholder="object type list (otl)*"
+      v-model="otl_input"
+    />
+    <input type="text" placeholder="OpenAI key" v-model="userskey" />
+  </div>
+
+  <div class="btn-container">
+    <button>Generate</button>
+    <button>Clear</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const selectedLanguage = ref("English");
+const selectedModel = ref("RE");
+
+const rtl_input = ref("");
+const stl_input = ref("");
+const otl_input = ref("");
+</script>
+
+<style scoped>
+.selector {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  margin-top: 1rem;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  margin-top: 2rem;
+}
+
+.content-container input {
+  width: 35rem;
+  height: 3rem;
+  border-radius: 3px;
+  border: 1px solid black;
+  padding: 0 10px;
+}
+
+.btn-container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 20px;
+}
+
+.btn-container button {
+  width: 10rem;
+  height: 3rem;
+  background-color: rgb(0, 0, 0);
+  border: 1px solid black;
+  border-radius: 5px;
+  transition: all 0.3s ease;
+  color: white;
+}
+
+.btn-container button:hover {
+  background-color: rgb(55, 55, 55);
+  cursor: pointer;
+}
+</style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -9,67 +9,39 @@
     </div>
 
     <div class="project-info">
-      It's a 'copy' version of GPT4IE and I carry out the engineering
+      It's a rebuilt version of GPT4IE and I carry out the engineering
       transformation of this project.
     </div>
-  </div>
 
-  <div class="selector">
-    <div class="radio-language">
-      <input type="radio" id="en" value="English" v-model="selectedLanguage" />
-      <label for="en">English</label>
-
-      <input type="radio" id="ch" value="Chinese" v-model="selectedLanguage" />
-      <label for="ch">中文</label>
+    <div style="margin-top: 5px;">
+      <a href="https://github.com/lavanceeee/GPT4IE-SelfImpl">this project's repository is here.</a>
     </div>
 
-    <div class="radio-model">
-      <input type="radio" id="RE" value="RE" v-model="selectedModel" />
-      <label for="RE">RE</label>
+    <table>
+      <tr>
+        <td>RE</td>
+        <td>entity-relation joint extraction</td>
+      </tr>
 
-      <input type="radio" id="NER" value="NER" v-model="selectedModel" />
-      <label for="RE">NER</label>
+      <tr>
+        <td>NER</td>
+        <td>named entity recoginzation</td>
+      </tr>
 
-      <input type="radio" id="EE" value="EE" v-model="selectedModel" />
-      <label for="RE">EE</label>
-    </div>
+      <tr>
+        <td>EE</td>
+        <td>event extraction</td>
+      </tr>
+    </table>
   </div>
 
-  <div class="content-container">
-    <input type="text" placeholder="sentence" v-model="sentence" />
-    <input
-      type="text"
-      placeholder="relation type list (rtl)*"
-      v-model="rtl_input"
-    />
-    <input
-      type="text"
-      placeholder="subject type list (stl)*"
-      v-model="stl_input"
-    />
-    <input
-      type="text"
-      placeholder="object type list (otl)*"
-      v-model="otl_input"
-    />
-    <input type="text" placeholder="OpenAI key" v-model="userskey" />
-  </div>
+  <MainContainer/>
 
-  <div class="btn-container">
-    <button>Generate</button>
-    <button>Clear</button>
-  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-
-const selectedLanguage = ref("English");
-const selectedModel = ref("RE");
-
-const rtl_input = ref("");
-const stl_input = ref("");
-const otl_input = ref("");
+import MainContainer from "./MainContainer.vue";
 </script>
 
 <style scoped>
@@ -94,55 +66,25 @@ const otl_input = ref("");
 }
 
 .top-container .project-info {
-  width: 25rem;
+  width: 35rem;
   font-size: 1.3rem;
   font-family: "Noticia Text";
   margin-top: 20px;
   text-align: center;
 }
 
-.selector {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 5px;
-  margin-top: 5rem;
-}
-
-.content-container {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  align-items: center;
-  margin-top: 2rem;
-}
-
-.content-container input {
-  width: 35rem;
-  height: 3rem;
-  border-radius: 3px;
-  border: 1px solid black;
-}
-
-.btn-container {
-  display: flex;
-  justify-content: center;
-  gap: 10px;
-  margin-top: 20px;
-}
-
-.btn-container button {
-  width: 10rem;
-  height: 3rem;
-  background-color: rgb(0, 0, 0);
-  border: 1px solid black;
-  border-radius: 5px;
-  transition: all 0.3s ease;
-  color: white;
-}
-
-.btn-container button:hover {
-  background-color: rgb(55, 55, 55);
-  cursor: pointer;
-}
+  table {
+    width: 35rem;
+    border-collapse: collapse;
+    margin-top: 20px;
+    font-family: Arial, sans-serif;
+    border-top: 2px solid #6969699a;
+    border-bottom: 2px solid #6969699a;
+  }
+  td {
+    padding: 20px 50px; 
+    text-align: center;
+    border-top: 1px solid #6666668c;
+    border-bottom: 1px solid #6666668c;
+  }
 </style>
