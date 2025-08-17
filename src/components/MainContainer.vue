26029a8a4c65ca4800965376516128a688954948
LiZhongpeng2 <lizhongpeng2@gmail.com>
2025-08-17 20:29:18 +0800

    fix: input box display when model changed

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,44 +1,134 @@
+<script setup>
+import { reactive, watch } from "vue";
+import { getUsersForm } from "../composables/useData";
+
+const selectedPattern = reactive({
+  selectedLanguage: "eng",
+  selectedModel: "RE",
+});
+
+const usersInputForm = reactive({
+  sentence: "",
+  usersKey: "",
+  rtl: "",
+  stl: "",
+  otl: "",
+});
+
+watch(
+  () => selectedPattern.selectedModel,
+  (newPattern) => {
+    if (newPattern === "RE") {
+      (usersInputForm.rtl = ""),
+        (usersInputForm.stl = ""),
+        (usersInputForm.otl = ""),
+        delete usersInputForm.etl;
+    } else {
+      (usersInputForm.etl = ""), delete usersInputForm.rtl;
+      delete usersInputForm.stl;
+      delete usersInputForm.otl;
+    }
+  },
+  { immediate: true }
+);
+
+const submmitInput = () => {
+  if (!usersInputForm.sentence) {
+    alert("Please input the sentence firstly!");
+    return;
+  }
+
+  //submmit users input
+  getUsersForm(selectedPattern, usersInputForm);
+};
+</script>
+
+
 <template>
   <div class="selector">
     <div class="radio-language">
-      <input type="radio" id="en" value="eng" v-model="selectedPattern.selectedLanguage" />
+      <input
+        type="radio"
+        id="en"
+        value="eng"
+        v-model="selectedPattern.selectedLanguage"
+      />
       <label for="en">English</label>
 
-      <input type="radio" id="ch" value="chi" v-model="selectedPattern.selectedLanguage" />
+      <input
+        type="radio"
+        id="ch"
+        value="chi"
+        v-model="selectedPattern.selectedLanguage"
+      />
       <label for="ch">Chinese</label>
     </div>
 
     <div class="radio-model">
-      <input type="radio" id="RE" value="RE" v-model="selectedPattern.selectedModel" />
+      <input
+        type="radio"
+        id="RE"
+        value="RE"
+        v-model="selectedPattern.selectedModel"
+      />
       <label for="RE">RE</label>
 
-      <input type="radio" id="NER" value="NER" v-model="selectedPattern.selectedModel" />
+      <input
+        type="radio"
+        id="NER"
+        value="NER"
+        v-model="selectedPattern.selectedModel"
+      />
       <label for="RE">NER</label>
 
-      <input type="radio" id="EE" value="EE" v-model="selectedPattern.selectedModel" />
+      <input
+        type="radio"
+        id="EE"
+        value="EE"
+        v-model="selectedPattern.selectedModel"
+      />
       <label for="RE">EE</label>
     </div>
   </div>
 
   <div class="content-container">
-    <input type="text" placeholder="sentence" v-model="usersInputForm.sentence" />
+    <input
+      type="text"
+      placeholder="sentence"
+      v-model="usersInputForm.sentence"
+    />
+
+    <template v-if="selectedPattern.selectedModel === 'RE'">
+      <input
+        type="text"
+        placeholder="relation type list (rtl)*"
+        v-model="usersInputForm.rtl"
+      />
+      <input
+        type="text"
+        placeholder="subject type list (stl)*"
+        v-model="usersInputForm.stl"
+      />
+      <input
+        type="text"
+        placeholder="object type list (otl)*"
+        v-model="usersInputForm.otl"
+      />
+    </template>
+
+    <template v-else>
+      <input
+        type="text"
+        placeholder="entity type list (etl)*"
+        v-model="usersInputForm.etl"
+      />
+    </template>
 
     <input
       type="text"
-      placeholder="relation type list (rtl)*"
-      v-model="usersInputForm.rtl"
+      placeholder="OpenAI key"
+      v-model="usersInputForm.usersKey"
     />
-    <input
-      type="text"
-      placeholder="subject type list (stl)*"
-      v-model="usersInputForm.stl"
-    />
-    <input
-      type="text"
-      placeholder="object type list (otl)*"
-      v-model="usersInputForm.otl"
-    />
-    <input type="text" placeholder="OpenAI key" v-model="usersInputForm.usersKey" />
   </div>
 
   <div class="btn-container">
@@ -47,43 +137,7 @@
   </div>
 </template>
 
-<script setup>
-import {ref, reactive} from 'vue';
-import { getUsersForm } from '../composables/useData';
 
-const selectedPattern = reactive({
-    selectedLanguage: "eng",
-    selectedModel: "RE"
-})
-
-const usersInputForm = reactive({
-    sentence: "",
-    rtl: "",
-    stl: "",
-    otl: "",
-    usersKey: ""
-})
-
-const submmitInput = () => {
-    if (!usersInputForm.sentence) {
-        alert("Please input the sentence firstly!");
-        return;
-    }
-
-    //submmit users input
-    getUsersForm(selectedPattern, usersInputForm);
-
-
-
-
-
-
-}
-
-
-
-
-</script>
 
 <style scoped>
 .selector {
